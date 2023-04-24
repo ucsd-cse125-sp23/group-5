5eb3768b483a44a358508036797e76a44e202722
Esa Hammado <ehammado@ucsd.edu>
2023-04-24 16:09:38 -0700

    scene graph fixes

--- client/src/scene.rs
+++ client/src/scene.rs
@@ -14,6 +14,20 @@ pub enum ModelIndices{
     FERRIS = 3,
 }
 
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum NodeID{
+    WORLD_NODE = 0,
+    PLAYER_NODE = 1,
+    ISLAND_NODE = 2,
+    TABLE_NODE = 3,
+    TABLE_TOP_NODE = 4,
+    TABLE_LEG1_NODE = 5,
+    TABLE_LEG2_NODE = 6,
+    TABLE_LEG3_NODE = 7,
+    TABLE_LEG4_NODE = 8,
+    FERRIS_NODE = 9,
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct ModelIndex{
     pub index: usize,
@@ -21,7 +35,7 @@ pub struct ModelIndex{
 
 #[derive(Clone, Debug)]
 pub struct Node {
-    pub childnodes: Vec<String>,
+    pub childnodes: Vec<NodeID>,
     pub models: Vec<(ModelIndex, Instance)>,
 }
 
@@ -37,7 +51,7 @@ impl Node{
 // #[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Scene{
     pub objects: Vec<model::Model>,
-    pub scene_graph: HashMap<String,(Node,Instance)>,
+    pub scene_graph: HashMap<NodeID,(Node,Instance)>,
     pub objects_and_instances: HashMap<ModelIndex, Vec<Instance>>,
 }
 
@@ -64,7 +78,7 @@ impl Scene{
         let mut matrix_stack: Vec<glm::TMat4<f32>> = Vec::new(); 
 
         // state needed for DFS:
-        let mut cur_node: &Node = &self.scene_graph.get("world").unwrap().0; // self.scene_graph.get("world").unwrap(); // should be the root of the tree to start --> "world"
+        let mut cur_node: &Node = &self.scene_graph.get(&NodeID::WORLD_NODE).unwrap().0; // self.scene_graph.get("world").unwrap(); // should be the root of the tree to start --> "world"
         let mut cur_VM: glm::TMat4<f32> = mat4_identity; //camera.calc_matrix(); // should be the camera's view matrix to start --> "world"s modelview matrix is the camera's view matrix
                                                                     // currently it's just the identity matrix!
         dfs_stack.push(cur_node);
@@ -135,43 +149,43 @@ impl Scene{
 
         let table_top_instance_m = Instance{transform: glm::translate( &mat4_identity, &glm::vec3(0.0,-0.1,0.0))* glm::scale( &mat4_identity, &glm::vec3(2.0,0.2,1.0))};
         table_top_node.models.push((ModelIndex{index: ModelIndices::CUBE as usize}, table_top_instance_m));
-        table_top_node.childnodes.push("ferris".to_string()); 
+        table_top_node.childnodes.push(NodeID::FERRIS_NODE); 
 
         let table_leg_instance_m = Instance{transform: glm::translate( &mat4_identity, &glm::vec3(0.0,0.5,0.0))* glm::scale( &mat4_identity, &glm::vec3(0.2,1.0,0.2))};
         table_leg_node.models.push((ModelIndex{index: ModelIndices::CUBE as usize}, table_leg_instance_m));
 
         let table_top_instance_c = Instance{transform: glm::translate( &mat4_identity, &glm::vec3(0.0,1.5,0.0))};
-        table_node.childnodes.push("table top".to_string());
+        table_node.childnodes.push(NodeID::TABLE_TOP_NODE);
         let table_leg_instance_1c = Instance{transform: glm::translate( &mat4_identity, &glm::vec3(-1.7,0.0,-0.7))};
-        table_node.childnodes.push("table leg 1".to_string());
+        table_node.childnodes.push(NodeID::TABLE_LEG1_NODE);
         let table_leg_instance_2c = Instance{transform: glm::translate( &mat4_identity, &glm::vec3(-1.7,0.0,0.7))};
-        table_node.childnodes.push("table leg 2".to_string());
+        table_node.childnodes.push(NodeID::TABLE_LEG2_NODE);
         let table_leg_instance_3c = Instance{transform: glm::translate( &mat4_identity, &glm::vec3(1.7,0.0,0.7))};
-        table_node.childnodes.push("table leg 3".to_string());
+        table_node.childnodes.push(NodeID::TABLE_LEG3_NODE);
         let table_leg_instance_4c = Instance{transform: glm::translate( &mat4_identity, &glm::vec3(1.7,0.0,-0.7))};
-        table_node.childnodes.push("table leg 4".to_string());
+        table_node.childnodes.push(NodeID::TABLE_LEG4_NODE);
 
         island_node.models.push((ModelIndex{index: ModelIndices::ISLAND as usize}, Instance{transform: mat4_identity}));
         let table_instance_c = Instance{transform: glm::rotate(&mat4_identity,-120.0*glm::pi::<f32>()/180.0, &glm::vec3(0.0, 1.0, 0.0)) * glm::translate( &mat4_identity, &glm::vec3(0.0,4.0,0.0))};
-        island_node.childnodes.push("table".to_string());
+        island_node.childnodes.push(NodeID::TABLE_NODE);
        
         let island_instance_c = Instance{transform: glm::translate( &mat4_identity, &glm::vec3(0.0,-9.7,0.0))};
-        world_node.childnodes.push("island".to_string());
+        world_node.childnodes.push(NodeID::ISLAND_NODE);
         
-        world_node.childnodes.push("player".to_string());
+        world_node.childnodes.push(NodeID::PLAYER_NODE);
         
         // println!("scene graph: {:?}", self.scene_graph);
 
-        self.scene_graph.insert("world".to_string(), (world_node.clone(), Instance{transform: mat4_identity}));
-        self.scene_graph.insert("ferris".to_string(), (ferris_node.clone(), Instance{transform: mat4_identity}));
-        self.scene_graph.insert("table top".to_string(), (table_top_node.clone(), table_top_instance_c));
-        self.scene_graph.insert("table leg 1".to_string(), (table_leg_node.clone(), table_leg_instance_1c));
-        self.scene_graph.insert("table leg 2".to_string(), (table_leg_node.clone(), table_leg_instance_2c));
-        self.scene_graph.insert("table leg 3".to_string(), (table_leg_node.clone(), table_leg_instance_3c));
-        self.scene_graph.insert("table leg 4".to_string(), (table_leg_node.clone(), table_leg_instance_4c));
-        self.scene_graph.insert("table".to_string(), (table_node.clone(), table_instance_c));
-        self.scene_graph.insert("island".to_string(), (island_node.clone(), island_instance_c));
-        self.scene_graph.insert("player".to_string(), (player_node.clone(), Instance{transform: mat4_identity}));
+        self.scene_graph.insert(NodeID::WORLD_NODE, (world_node.clone(), Instance{transform: mat4_identity}));
+        self.scene_graph.insert(NodeID::FERRIS_NODE, (ferris_node.clone(), Instance{transform: mat4_identity}));
+        self.scene_graph.insert(NodeID::TABLE_TOP_NODE, (table_top_node.clone(), table_top_instance_c));
+        self.scene_graph.insert(NodeID::TABLE_LEG1_NODE, (table_leg_node.clone(), table_leg_instance_1c));
+        self.scene_graph.insert(NodeID::TABLE_LEG2_NODE, (table_leg_node.clone(), table_leg_instance_2c));
+        self.scene_graph.insert(NodeID::TABLE_LEG3_NODE, (table_leg_node.clone(), table_leg_instance_3c));
+        self.scene_graph.insert(NodeID::TABLE_LEG4_NODE, (table_leg_node.clone(), table_leg_instance_4c));
+        self.scene_graph.insert(NodeID::TABLE_NODE, (table_node.clone(), table_instance_c));
+        self.scene_graph.insert(NodeID::ISLAND_NODE, (island_node.clone(), island_instance_c));
+        self.scene_graph.insert(NodeID::PLAYER_NODE, (player_node.clone(), Instance{transform: mat4_identity}));
         
     }
 }
