6b1b98ad46b24fe2df7d21558f3db1686d97aae6
YunxiangChi <chi_yunxiang@163.com>
2023-04-27 09:32:16 -0700

    resolve movement conflict

--- client/src/inputs/mod.rs
+++ client/src/inputs/mod.rs
@@ -57,10 +57,10 @@ impl InputEventProcessor {
     pub fn map_key(virtual_keycode: VirtualKeyCode) -> Option<(GameKeyKind, Command)> {
         match virtual_keycode {
             // match Holdable keys
-            VirtualKeyCode::W => {Some((GameKeyKind::Holdable, Command::Move(MoveDirection::Forward)))}
-            VirtualKeyCode::A => Some((GameKeyKind::Holdable, Command::Move(MoveDirection::Left))),
-            VirtualKeyCode::S => Some((GameKeyKind::Holdable, Command::Move(MoveDirection::Backward))),
-            VirtualKeyCode::D => Some((GameKeyKind::Holdable, Command::Move(MoveDirection::Right))),
+            VirtualKeyCode::W => Some((GameKeyKind::Holdable, Command::Move(vec3(0., 0., 1.)))),
+            VirtualKeyCode::A => Some((GameKeyKind::Holdable, Command::Move(vec3(1., 0., 0.)))),
+            VirtualKeyCode::S => Some((GameKeyKind::Holdable, Command::Move(vec3(0., 0., -1.)))),
+            VirtualKeyCode::D => Some((GameKeyKind::Holdable, Command::Move(vec3(-1., 0., 0.)))),
             // match Pressable keys
             VirtualKeyCode::Space => Some((GameKeyKind::Pressable, Jump)),
             VirtualKeyCode::LShift => Some((GameKeyKind::Pressable, Spawn)),
