47cb4c3e6328396f2dd9e89f7b0795e71a020c31
Xiyan <xiyan128@gmail.com>
2023-04-28 15:02:17 -0700

    fix(client): removed unnecessary rebuild

--- client/build.rs
+++ client/build.rs
@@ -4,15 +4,5 @@ use fs_extra::dir::CopyOptions;
 use std::env;
 
 fn main() -> Result<()> {
-    // This tells cargo to rerun this script if something in /res/ changes.
-    println!("cargo:rerun-if-changed=res/*");
-
-    let out_dir = env::var("OUT_DIR")?;
-    let mut copy_options = CopyOptions::new();
-    copy_options.overwrite = true;
-    let mut paths_to_copy = Vec::new();
-    paths_to_copy.push("res/");
-    copy_items(&paths_to_copy, out_dir, &copy_options)?;
-
     Ok(())
 }
