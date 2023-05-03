fc1ae3c362d803cea0ad57d2b85e356c2fdd0fee
Esa Hammado <ehammado@ucsd.edu>
2023-05-03 15:42:01 -0700

    small fixes

--- client/src/event_loop.rs
+++ client/src/event_loop.rs
@@ -38,8 +38,7 @@ impl PlayerLoop {
         let mut event_loop = EventLoop::new();
         let window = WindowBuilder::new()
             .with_title("As The Wind Blows")
-            // TODO: uncomment this line
-            //.with_fullscreen(Some(winit::window::Fullscreen::Borderless(Option::None)))
+            .with_fullscreen(Some(winit::window::Fullscreen::Borderless(Option::None)))
             .build(&event_loop)
             .unwrap();
 
