0294fbb8e276b988b2a4569a6174edeeab5fe2b5
YunxiangChi <78146435+YunxiangChi@users.noreply.github.com>
2023-05-18 11:24:33 -0700

    adjust impulse

--- common/src/configs/parameters.rs
+++ common/src/configs/parameters.rs
@@ -55,14 +55,14 @@ pub const INVINCIBLE_EFFECTIVE_DISTANCE: f32 = 2.5;
 pub const INVINCIBLE_EFFECTIVE_IMPULSE: f32 = 20.0;
 pub const MAX_ATTACK_DIST: f32 = 10.0;
 pub const MAX_ATTACK_ANGLE: f32 = std::f32::consts::FRAC_PI_6;
-pub const ATTACK_IMPULSE: f32 = 100.0;
+pub const ATTACK_IMPULSE: f32 = 250.0;
 // attack impulse force
 pub const ATTACK_COEFF: f32 = 5.0;
 // controls impact of distance on normal attack falloff (higher = distance matters more)
 pub const ATTACK_COST: u32 = 1;
 pub const ATTACK_COOLDOWN: f32 = 1.0;
 pub const MAX_AREA_ATTACK_DIST: f32 = 5.0;
-pub const AREA_ATTACK_IMPULSE: f32 = 75.0;
+pub const AREA_ATTACK_IMPULSE: f32 = 150.0;
 // area attack impulse force
 pub const AREA_ATTACK_COEFF: f32 = 5.0;
 // controls impact of distance on area attack falloff (higher = distance matters more)
