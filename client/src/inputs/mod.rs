7936e76cf6f1b9c7696249d8d29385f97a6d0983
Binghong-Leo-Li <97146983+Binghong-Leo-Li@users.noreply.github.com>
2023-05-23 11:19:41 -0700

    fixed

--- client/src/inputs/mod.rs
+++ client/src/inputs/mod.rs
@@ -79,7 +79,7 @@ impl InputEventProcessor {
             VirtualKeyCode::Space => Some((GameKeyKind::Pressable, Jump)),
             VirtualKeyCode::LShift => Some((GameKeyKind::Pressable, Spawn)),
             VirtualKeyCode::RShift => Some((GameKeyKind::Pressable, Die)),
-            VirtualKeyCode::C => Some((GameKeyKind::Pressable, CastPowerUp)), // TODO: Change
+            VirtualKeyCode::E => Some((GameKeyKind::Pressable, CastPowerUp)), // TODO: Change
 
             // match PressRelease keys
             // VirtualKeyCode::LShift => Some((GameKeyKind::PressRelease, Spawn)),
