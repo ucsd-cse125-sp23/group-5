0d2730c342f797537ca93b5595a1c63017ffb854
Alan Wang <alanwangsd@gmail.com>
2023-05-22 21:56:37 -0700

    make profile body follow lobby color

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -854,150 +854,178 @@ impl State {
                     dt,
                     self.client_id,
                 );
+            
+            // only update game-related info if we're in game 
+            if let GameLifeCycleState::Running(timestamp) = game_state_clone.life_cycle_state {
+                other_players::load_game_state(
+                    &mut self.other_players,
+                    game_state.lock().unwrap(),
+                    game_config.clone(),
+                );
 
-            other_players::load_game_state(
-                &mut self.other_players,
-                game_state.lock().unwrap(),
-                game_config.clone(),
-            );
+                // update player scores
+                {
+                    let screen_id = self
+                        .display
+                        .groups
+                        .get(&self.display.game_display)
+                        .unwrap()
+                        .screen
+                        .as_ref()
+                        .unwrap();
 
-            // update player scores
-            {
-                let screen_id = self
-                    .display
-                    .groups
-                    .get(&self.display.game_display)
-                    .unwrap()
-                    .screen
-                    .as_ref()
-                    .unwrap();
+                    let screen = self.display.screen_map.get_mut(screen_id).unwrap();
+                    for i in 1..5 {
+                        let score_ind = *screen
+                            .icon_id_map
+                            .get(&format!("icon:score_p{}", i))
+                            .unwrap();
+                        let profile_body_ind = *screen
+                            .icon_id_map
+                            .get(&format!("icon:profile_body_p{}",i))
+                            .unwrap();
+                        let profile_leaf_ind = *screen
+                            .icon_id_map
+                            .get(&format!("icon:profile_leaf_p{}",i))
+                            .unwrap();
+                        let score: f32 = self.other_players[i as usize - 1].score;
+                        
+                        // Set colors/textures 
+                        if let Some(player_customization) = game_state_clone.players_customization.get(&i) {
+                            let body_color = player_customization.color.get("korok").unwrap().rgb_color;
+                            screen.icons[profile_body_ind].tint = glm::vec4(body_color[0], body_color[1], body_color[2], 1.0);
+                        } 
 
-                let screen = self.display.screen_map.get_mut(screen_id).unwrap();
-                for i in 1..5 {
-                    let score_ind = *screen
-                        .icon_id_map
-                        .get(&format!("icon:score_p{}", i))
+                        for ind in [score_ind, profile_body_ind, profile_leaf_ind] {
+                            let mut location = screen.icons[ind].location;
+                            location.horz_disp = (
+                                0.0,
+                                game_config.score_lower_x
+                                    + score * (game_config.score_upper_x - game_config.score_lower_x),
+                            );
+                            screen.icons[ind].relocate(
+                                location,
+                                self.config.width,
+                                self.config.height,
+                                &self.queue,
+                            );
+                        }
+                    }
+                }
+
+                // update player number of charges
+                {
+                    let screen_id = self
+                        .display
+                        .groups
+                        .get(&self.display.game_display)
+                        .unwrap()
+                        .screen
+                        .as_ref()
                         .unwrap();
-                    let profile_body_ind = *screen
-                        .icon_id_map
-                        .get(&format!("icon:profile_body_p{}",i))
+
+                    let screen = self.display.screen_map.get_mut(screen_id).unwrap();
+                    let ind = *screen.icon_id_map.get("icon:charge").unwrap();
+                    screen.icons[ind].inst_range = 0..self.player.wind_charge;
+                }
+
+                // update weather icon
+                {
+                    let screen_id = self
+                        .display
+                        .groups
+                        .get(&self.display.game_display)
+                        .unwrap()
+                        .screen
+                        .as_ref()
                         .unwrap();
-                    let profile_leaf_ind = *screen
-                        .icon_id_map
-                        .get(&format!("icon:profile_leaf_p{}",i))
+
+                    let screen = self.display.screen_map.get_mut(screen_id).unwrap();
+                    let wind_ind = *screen.icon_id_map.get("icon:windy").unwrap();
+
+                    screen.icons[wind_ind].inst_range = 0..{
+                        if matches!(
+                            game_state.lock().unwrap().world.weather,
+                            Some(Weather::Windy(_))
+                        ) {
+                            1
+                        } else {
+                            0
+                        }
+                    };
+
+                    let rain_ind = *screen.icon_id_map.get("icon:rainy").unwrap();
+                    screen.icons[rain_ind].inst_range = 0..{
+                        if matches!(
+                            game_state.lock().unwrap().world.weather,
+                            Some(Weather::Rainy)
+                        ) {
+                            1
+                        } else {
+                            0
+                        }
+                    };
+                }
+
+                // update cooldowns
+                // hard coded for now... TODO: separate function
+                // is it necessary? would need to pass around lots of references
+                // might be better to create dedicated function in screen/command_handlers
+                {
+                    let screen_id = self
+                        .display
+                        .groups
+                        .get(&self.display.game_display)
+                        .unwrap()
+                        .screen
+                        .as_ref()
                         .unwrap();
-                    let score: f32 = self.other_players[i as usize - 1].score;
-                    
-                    for ind in [score_ind, profile_body_ind, profile_leaf_ind] {
-                        let mut location = screen.icons[ind].location;
-                        location.horz_disp = (
-                            0.0,
-                            game_config.score_lower_x
-                                + score * (game_config.score_upper_x - game_config.score_lower_x),
+
+                    // TODO: Magic constants here seem a little unavoidable?
+                    let atk_load = String::from("icon:atk_forward_overlay");
+                    let atk_area_load = String::from("icon:atk_wave_overlay");
+
+                    if self.player.on_cooldown.contains_key(&Command::Attack) {
+                        let cd_left = self.player.on_cooldown.get(&Command::Attack).unwrap()
+                            / physics_config.attack_config.attack_cooldown;
+                        self.display.transition_map.insert(
+                            atk_load.clone(),
+                            screen::object_transitions::Transition::SqueezeDown(cd_left),
                         );
-                        screen.icons[ind].relocate(
-                            location,
-                            self.config.width,
-                            self.config.height,
-                            &self.queue,
+                    } else {
+                        self.display.transition_map.remove(&atk_load);
+                    }
+
+                    if self.player.on_cooldown.contains_key(&Command::AreaAttack) {
+                        let cd_left = self.player.on_cooldown.get(&Command::AreaAttack).unwrap()
+                            / physics_config.attack_config.area_attack_cooldown;
+                        self.display.transition_map.insert(
+                            atk_area_load.clone(),
+                            screen::object_transitions::Transition::SqueezeDown(cd_left),
                         );
-                    }
-                }
-            }
-
-            // update player number of charges
-            {
-                let screen_id = self
-                    .display
-                    .groups
-                    .get(&self.display.game_display)
-                    .unwrap()
-                    .screen
-                    .as_ref()
-                    .unwrap();
-
-                let screen = self.display.screen_map.get_mut(screen_id).unwrap();
-                let ind = *screen.icon_id_map.get("icon:charge").unwrap();
-                screen.icons[ind].inst_range = 0..self.player.wind_charge;
-            }
-
-            // update weather icon
-            {
-                let screen_id = self
-                    .display
-                    .groups
-                    .get(&self.display.game_display)
-                    .unwrap()
-                    .screen
-                    .as_ref()
-                    .unwrap();
-
-                let screen = self.display.screen_map.get_mut(screen_id).unwrap();
-                let wind_ind = *screen.icon_id_map.get("icon:windy").unwrap();
-
-                screen.icons[wind_ind].inst_range = 0..{
-                    if matches!(
-                        game_state.lock().unwrap().world.weather,
-                        Some(Weather::Windy(_))
-                    ) {
-                        1
                     } else {
-                        0
+                        self.display.transition_map.remove(&atk_area_load);
                     }
-                };
+                }
 
-                let rain_ind = *screen.icon_id_map.get("icon:rainy").unwrap();
-                screen.icons[rain_ind].inst_range = 0..{
-                    if matches!(
-                        game_state.lock().unwrap().world.weather,
-                        Some(Weather::Rainy)
-                    ) {
-                        1
-                    } else {
-                        0
-                    }
-                };
-            }
-
-            // update cooldowns
-            // hard coded for now... TODO: separate function
-            // is it necessary? would need to pass around lots of references
-            // might be better to create dedicated function in screen/command_handlers
-            {
-                let screen_id = self
+                let player_loc = self
                     .display
-                    .groups
-                    .get(&self.display.game_display)
+                    .scene_map
+                    .get(scene_id)
                     .unwrap()
-                    .screen
-                    .as_ref()
-                    .unwrap();
+                    .get_player_positions();
 
-                // TODO: Magic constants here seem a little unavoidable?
-                let atk_load = String::from("icon:atk_forward_overlay");
-                let atk_area_load = String::from("icon:atk_wave_overlay");
-
-                if self.player.on_cooldown.contains_key(&Command::Attack) {
-                    let cd_left = self.player.on_cooldown.get(&Command::Attack).unwrap()
-                        / physics_config.attack_config.attack_cooldown;
-                    self.display.transition_map.insert(
-                        atk_load.clone(),
-                        screen::object_transitions::Transition::SqueezeDown(cd_left),
-                    );
-                } else {
-                    self.display.transition_map.remove(&atk_load);
+                // ASSUME: Ids should always be 1-4
+                for p in &mut self.other_players {
+                    p.visible = false;
+                }
+                for (i, loc) in player_loc {
+                    self.other_players[i as usize - 1].location = loc;
+                    self.other_players[i as usize - 1].visible = true;
                 }
 
-                if self.player.on_cooldown.contains_key(&Command::AreaAttack) {
-                    let cd_left = self.player.on_cooldown.get(&Command::AreaAttack).unwrap()
-                        / physics_config.attack_config.area_attack_cooldown;
-                    self.display.transition_map.insert(
-                        atk_area_load.clone(),
-                        screen::object_transitions::Transition::SqueezeDown(cd_left),
-                    );
-                } else {
-                    self.display.transition_map.remove(&atk_area_load);
-                }
+                self.invisible_players = game_state_clone.get_affected_players(StatusEffect::Invisible);
+                self.existing_powerups = game_state_clone.get_existing_powerups();
             }
 
             self.display
@@ -1005,25 +1033,6 @@ impl State {
                 .get_mut(scene_id)
                 .unwrap()
                 .draw_scene_dfs();
-
-            let player_loc = self
-                .display
-                .scene_map
-                .get(scene_id)
-                .unwrap()
-                .get_player_positions();
-
-            // ASSUME: Ids should always be 1-4
-            for p in &mut self.other_players {
-                p.visible = false;
-            }
-            for (i, loc) in player_loc {
-                self.other_players[i as usize - 1].location = loc;
-                self.other_players[i as usize - 1].visible = true;
-            }
-
-            self.invisible_players = game_state_clone.get_affected_players(StatusEffect::Invisible);
-            self.existing_powerups = game_state_clone.get_existing_powerups();
         }
 
         let particle_queue = particle_queue.lock().unwrap();
