dde12256a34993818c9ab1939b15a70a983f0bac
Alan Wang <alanwangsd@gmail.com>
2023-06-06 16:07:24 -0700

    add rainly font

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -12,7 +12,7 @@ use glm::vec3;
 use nalgebra_glm as glm;
 use nalgebra_glm::{TVec3, Vec3};
 use wgpu::util::DeviceExt;
-use wgpu_glyph::{ab_glyph, GlyphBrush, GlyphBrushBuilder, HorizontalAlign, Layout, Section, Text};
+use wgpu_glyph::{ab_glyph, FontId, GlyphBrush, GlyphBrushBuilder, HorizontalAlign, Layout, Section, Text};
 use winit::event::*;
 use winit::window::Window;
 
@@ -574,8 +574,13 @@ impl State {
             "../../assets/Inconsolata-Regular.ttf"
         ))
         .unwrap();
+        let rainly = ab_glyph::FontArc::try_from_slice(include_bytes!(
+            "../../assets/Rainly Regular.ttf"
+        ))
+        .unwrap();
+        let fonts_vec = vec![inconsolata, rainly]; 
 
-        let glyph_brush = GlyphBrushBuilder::using_font(inconsolata).build(&device, surface_format);
+        let glyph_brush = GlyphBrushBuilder::using_fonts(fonts_vec).build(&device, surface_format);
 
         let rng = rand::thread_rng();
         let particle_tex =
@@ -1402,12 +1407,13 @@ impl State {
             self.animation_controller
                 .play_animation("idle".to_string(), "object:player_model".to_string());
             self.glyph_brush.queue(Section {
-                screen_position: (size.width as f32 * 0.25, size.height as f32 * 0.9),
+                screen_position: (size.width as f32 * 0.15, size.height as f32 * 0.875),
                 bounds: (size.width as f32, size.height as f32),
                 text: vec![
-                    Text::new(format!("READY! WAITING ON OTHER PLAYERS...").as_str())
+                    Text::new(format!("READY! WAITING FOR OTHER PLAYERS...").as_str())
                         .with_color([0.0, 0.0, 0.0, 1.0])
-                        .with_scale(40.0),
+                        .with_scale(90.0)
+                        .with_font_id(FontId(1)),
                 ],
                 ..Section::default()
             });
@@ -1428,20 +1434,21 @@ impl State {
             if self.player.on_cooldown.contains_key(&Command::Spawn) {
                 let spawn_cooldown = self.player.on_cooldown.get(&Command::Spawn).unwrap();
                 self.glyph_brush.queue(Section {
-                    screen_position: (size.width as f32 * 0.5, size.height as f32 * 0.4),
+                    screen_position: (size.width as f32 * 0.5, size.height as f32 * 0.35),
                     bounds: (size.width as f32, size.height as f32),
                     text: vec![
-                        Text::new("You died!\n")
-                            .with_color([1.0, 1.0, 0.0, 1.0])
-                            .with_scale(100.0),
+                        Text::new("YOU DIED!\n")
+                            .with_color([0.867, 0.667, 0.0, 1.0])
+                            .with_scale(200.0)
+                            .with_font_id(FontId(1)),
                         Text::new("Respawning in ")
-                            .with_color([1.0, 1.0, 0.0, 1.0])
+                            .with_color([0.867, 0.667, 0.0, 1.0])
                             .with_scale(60.0),
                         Text::new(format!("{:.1}", spawn_cooldown).as_str())
-                            .with_color([1.0, 1.0, 1.0, 1.0])
+                            .with_color([0.867, 1.0, 1.0, 1.0])
                             .with_scale(60.0),
                         Text::new(" seconds")
-                            .with_color([1.0, 1.0, 0.0, 1.0])
+                            .with_color([0.867, 0.667, 0.0, 1.0])
                             .with_scale(60.0),
                     ],
                     layout: Layout::default().h_align(HorizontalAlign::Center),
